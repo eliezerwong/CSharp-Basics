d0b5f2ddc3b577e4b12ca20fc3daf9bde2a34c9e
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2021-11-12 21:43:15 -0600

    Add files via upload

---
+++ homework1-eliezerwong/Homework 1/Homework 1/Program.cs
@@ -0,0 +1,40 @@
+﻿//Eliezer Emmanuel Wong
+//MIS 3013-995
+using System;
+
+namespace SalesTotal
+{   /*
+    1. Prompt the user to enter what type of item they are purchasing
+    2. Prompt the user to enter how many of <item>
+    3. Prompt the user for the price of one item
+    4. Calculate the subtotal, the sales tax and the sales total (subtotal + sales tax) and output all 3 to the user.  Assume that the sales tax for your application is 8.5% (create a constant to store this value and use the constant in your calculations).
+     */
+    class Program
+    {
+        const double tax = 0.085;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- SalesTotal ---\n\r");
+            //1.
+            Console.WriteLine("What is the name of the item you are purchasing?");
+            string itemName = Console.ReadLine();
+            //2.
+            Console.WriteLine($"How many {itemName} do you want to buy?");
+            int itemQuant = Convert.ToInt32(Console.ReadLine());
+            //3.
+            Console.WriteLine("What is the price for each {0}?", itemName);
+            double itemPrice = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\n");
+            //4.
+            double subTotal = itemQuant * itemPrice;
+            double salesTax = subTotal * tax;
+            Console.WriteLine($"\tYour subtotal for your bill is {subTotal:C2}");
+            Console.WriteLine($"\tYour sales tax for your bill is {salesTax:C2}");
+            Console.WriteLine($"\tYour total for your bill is {(subTotal+salesTax):C2}\n\r");
+
+            Console.WriteLine("Press any key to continue . . .");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ homework1-eliezerwong/studentgrade/homework 1/Program.cs
@@ -0,0 +1,57 @@
+﻿//Eliezer Emmanuel Wong
+//MIS 3013-995
+using System;
+
+namespace StudentGrade
+{
+    /*
+    1. Prompt the user to enter their first name, their last name, their student id number as well as their overall percentage grade for quizzes, participations, homeworks, midterm and final exam. 
+    2. Use the weighted percentages on the syllabus for this class to calculate their final grade and output it as a percent.
+     */
+    class Program
+    {
+        const double hC = 0.25;
+        const double pC = 0.1;
+        const double qC = 0.1;
+        const double mC = 0.25;
+        const double fC = 0.3;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- StudentGrade ---");
+
+            //1.
+            Console.WriteLine("What is your first name?");
+            string firstN = Console.ReadLine();
+            
+            Console.WriteLine("What is your last name?");
+            string lastN = Console.ReadLine();
+
+            Console.WriteLine("What is your student ID?");
+            int idNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for homeworks?");
+            double hG = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for participation?");
+            double pG = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            double qG = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for the midterm?");
+            double mG = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for the final?");
+            double fG = Convert.ToDouble(Console.ReadLine());
+
+            //2.
+            double finalG = (hG*hC) + (pG*pC) + (qG*qC) + (mG*mC) + (fG*fC); 
+            Console.WriteLine($"{firstN} {lastN} ({idNum}), your final grade is {(finalG/100):P2}.\n\r");
+
+            Console.WriteLine("Press any key to continue . . .");
+
+            Console.ReadKey();
+        }
+    }
+}
