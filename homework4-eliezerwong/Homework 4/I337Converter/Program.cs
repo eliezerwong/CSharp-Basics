1000d4672dc33ad11e5dc29e3ac6c89ef27213ef
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2021-11-12 21:45:22 -0600

    Add files via upload

---
+++ homework4-eliezerwong/Homework 4/I337Converter/Program.cs
@@ -0,0 +1,48 @@
+﻿//Eliezer Emmanuel Wong
+//MIS 3103-995
+using System;
+
+namespace I337Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = "--- I337Converter ---";
+            Console.SetCursorPosition((Console.WindowWidth - title.Length) / 2, Console.CursorTop);
+            Console.WriteLine(title);
+
+            Console.WriteLine("Input a sentence");
+            string userSentence = Console.ReadLine();
+            
+            //string manipulation to replace characters, *is it possible to Replace(array1, array2)? how to optimize?
+            userSentence = userSentence.ToUpper().Replace("A","@").Replace("E","3").Replace("H", "|-|").Replace("S", "$").Replace("T", "7").Replace("U", "|_|");
+
+            //make sure each sentence ends with !
+            userSentence = userSentence.Replace("?", "!").Replace(".", "!");
+            Console.WriteLine(userSentence);
+
+            /* or,
+            string[] sentence = userSentence.Split(new char[] {'.', '?' });
+            int sentenceCount = sentence.Length;
+
+            int i = 0;
+            bool hasChar = sentence[i].Contains("!");
+
+            for (i = 0; i < sentenceCount; i++)
+            {
+                if(hasChar)
+                {
+                    Console.Write(sentence[i] + " ");
+                }
+                else
+                {
+                    Console.Write(sentence[i] + "!");
+                }
+            }
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ homework4-eliezerwong/Homework 4/VowelsAndConsonants/Program.cs
@@ -0,0 +1,37 @@
+﻿//Eliezer Emmanuel Wong
+//MIS 3013-995
+using System;
+
+namespace VowelsAndConsonants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = "--- Vowels and Consonants ---";
+            Console.SetCursorPosition((Console.WindowWidth - title.Length) / 2, Console.CursorTop);
+            Console.WriteLine(title);
+
+            //user input
+            Console.WriteLine("Input a sentence");
+            string userSentence = Console.ReadLine();
+            
+            //string manipulation to more readable
+            userSentence = userSentence.Trim().ToLower().Replace(" ","").Replace(".","");
+            int characterCount = userSentence.Length;
+
+            //when using array without specifying, eg. cw(vowels);, all values output
+            char[] vowel = { 'a', 'e', 'i', 'o', 'u' };
+            
+            //does it before or after vowel?
+            string[] splitVowel = userSentence.Split(vowel);
+            int vowelCount = splitVowel.Length - 1;
+            int consonantCount = characterCount - vowelCount;
+
+            Console.WriteLine($"There were {vowelCount} vowels in the sentence.");
+            Console.WriteLine($"There were {consonantCount} consonants in the sentence.");
+
+            Console.ReadKey();
+        }
+    }
+}
